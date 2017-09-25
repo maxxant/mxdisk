3bba7fdfed609bf67430589df1b62e917cde0a23
maxxant <maxxant@gmail.com>
2017-09-25 20:14:35 +0300

    up

--- diskbyx.go
+++ diskbyx.go
@@ -6,31 +6,6 @@ import (
 	"path/filepath"
 )
 
-// path:
-//  /dev/disk/by-uuid
-//  /dev/disk/by-label
-//  /dev/disk/by-path
-// returns map [by-xxx] /dev/sdxN
-// NOTE: not all OS supports path "by-label"
-func disksByPathX(path string) map[string]string {
-	mp := make(map[string]string)
-	filepath.Walk(path, func(path string, inf os.FileInfo, err error) error {
-		if err != nil {
-			return err // if path is not exists
-		}
-		if inf.IsDir() {
-			return nil
-		}
-		if (inf.Mode() & os.ModeSymlink) != 0 {
-			link := filepath.Base(path)
-			name, _ := filepath.EvalSymlinks(path)
-			mp[name] = link
-		}
-		return err
-	})
-	return mp
-}
-
 const (
 	byUUID  = iota
 	byLabel = iota
@@ -42,6 +17,39 @@ type disksByX struct {
 	uuid  map[string]string
 	label map[string]string
 	path  map[string]string
+}
+
+// newDisksByX for paths:
+// - /dev/disk/by-uuid
+// - /dev/disk/by-label
+// - /dev/disk/by-path
+// returns map [by-xxx] /dev/sdxN
+// NOTE: not all OS supports path "by-label"
+func newDisksByX() *disksByX {
+	fill4path := func(path string) map[string]string {
+		mp := make(map[string]string)
+		filepath.Walk(path, func(path string, inf os.FileInfo, err error) error {
+			if err != nil {
+				return err // if path is not exists
+			}
+			if inf.IsDir() {
+				return nil
+			}
+			if (inf.Mode() & os.ModeSymlink) != 0 {
+				link := filepath.Base(path)
+				name, _ := filepath.EvalSymlinks(path)
+				mp[name] = link
+			}
+			return err
+		})
+		return mp
+	}
+
+	return &disksByX{
+		uuid:  fill4path("/dev/disk/by-uuid"),
+		label: fill4path("/dev/disk/by-label"),
+		path:  fill4path("/dev/disk/by-path"),
+	}
 }
 
 func (p disksByX) getMap(byXFilter int) map[string]string {
@@ -67,21 +75,13 @@ func (p disksByX) find(byXFilter int, need string) string {
 func (p disksByX) String() string {
 	var s string
 	for k, v := range p.uuid {
-		s += fmt.Sprintf("uuid: %v:%v\n", k, v)
+		s += fmt.Sprintf("uuid: %v : %v\n", k, v)
 	}
 	for k, v := range p.label {
-		s += fmt.Sprintf("label: %v:%v\n", k, v)
+		s += fmt.Sprintf("label: %v : %v\n", k, v)
 	}
 	for k, v := range p.path {
-		s += fmt.Sprintf("path: %v:%v\n", k, v)
+		s += fmt.Sprintf("path: %v : %v\n", k, v)
 	}
 	return s
-}
-
-func newDisksByX() *disksByX {
-	return &disksByX{
-		uuid:  disksByPathX("/dev/disk/by-uuid"),
-		label: disksByPathX("/dev/disk/by-label"),
-		path:  disksByPathX("/dev/disk/by-path"),
-	}
 }
